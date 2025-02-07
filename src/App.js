87d2ed2f5b1f821ebed20f4a44e5dd78b43a022e
unknown <radhakrishnan.amarnath.tivagaram@gmail.com>
2025-02-07 15:22:22 +0530

    my second new commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React js
+          Learn React js new development
         </a>
       </header>
     </div>
