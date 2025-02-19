67e3a996b3ee8d5a6fdf419d5ed59034e8bf9760
unknown <radhakrishnan.amarnath.tivagaram@gmail.com>
2025-02-19 23:19:00 +0530

    my 19/2 new commit

--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -3,28 +3,35 @@ import React from 'react'
 import { IoIosMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { BsShop } from "react-icons/bs";
+import { FaShopify } from "react-icons/fa";
+import { GiChickenOven } from "react-icons/gi";
+import { FaMobileScreenButton } from "react-icons/fa6";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { GrBlog } from "react-icons/gr";
+import { MdContactMail } from "react-icons/md";
 
 const Navigation = () => {
   return (
     <nav>
           <div className='container'>
             <div className='row'>
-              <div className='col-sm-3 catogertyTab1'>
+              <div className='col-sm-2 catogertyTab1'>
                 <Button className='allCatTab'>
                   <span className='allCatIcon1'><IoIosMenu/></span>
                   <span class="text">All Categories</span>
                   <span className='allCatIcon2'><FaAngleDown/></span>
                 </Button>
               </div>
-              <div className='col-sm-8 catogertyTab2 d-flex align-items-center'>
+              <div className='col-sm-9 catogertyTab2 d-flex align-items-center'>
                 <ul className='list list-inline ml-auto'>
-                    <li className='list-inline-item'><Link to="/">Home</Link></li>
-                    <li className='list-inline-item'><Link to="/">Fashion</Link></li>
-                    <li className='list-inline-item'><Link to="/">Electronics</Link></li>
-                    <li className='list-inline-item'><Link to="/">Bakery</Link></li>
-                    <li className='list-inline-item'><Link to="/">Grocery</Link></li>
-                    <li className='list-inline-item'><Link to="/">Blog</Link></li>
-                    <li className='list-inline-item'><Link to="/">Contact us</Link></li>
+                    <li className='list-inline-item'><Link to="/"><BsShop/> &nbsp; Home</Link></li>
+                    <li className='list-inline-item'><Link to="/"><FaShopify/> &nbsp; Fashion</Link></li>
+                    <li className='list-inline-item'><Link to="/"><FaMobileScreenButton/> &nbsp; Electronics</Link></li>
+                    <li className='list-inline-item'><Link to="/"><GiChickenOven/> &nbsp; Foods</Link></li>
+                    <li className='list-inline-item'><Link to="/"><MdOutlineLocalGroceryStore/> &nbsp; Groceries</Link></li>
+                    <li className='list-inline-item'><Link to="/"><GrBlog/> &nbsp; Blog</Link></li>
+                    <li className='list-inline-item'><Link to="/"><MdContactMail/> &nbsp; Contact us</Link></li>
                 </ul>
               </div>
             </div>
