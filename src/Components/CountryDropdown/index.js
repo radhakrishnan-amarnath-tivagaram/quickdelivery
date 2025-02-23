78172165717d6e28da65a7af999101e01bb6d37d
unknown <radhakrishnan.amarnath.tivagaram@gmail.com>
2025-02-23 20:57:30 +0530

    my 23/2 new commit

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,23 +1,64 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { FaAngleDown } from "react-icons/fa";
-import DialogTitle from '@mui/material/DialogTitle';
+// import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
+// import Searchbar from '../Header/Searchbar';
+import { IoIosSearch } from 'react-icons/io'
+import { IoIosCloseCircle } from "react-icons/io";
+import { TransitionProps } from '@mui/material/transitions';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+      children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+  ) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 
 const CountryDropdown = () => {
+
+  const [isCountryModal,setisCountryModal] = useState(false)
+
   return (<div>
-        <Button className='countryDropdown'>
-            <div className='info d-flex flex-column'>
-                <span className='label'>Your Location</span>
-                <span className='name'>India</span>
-            </div>
-            <span className='ml-auto'><FaAngleDown/></span>
-        </Button>
-        
-        <Dialog open={true}>
-              <DialogTitle>Set backup account</DialogTitle>
-              <p>sfsdfd</p>
-        </Dialog>
+            <Button className='countryDropdown' onClick={()=>setisCountryModal(true)}>
+                <div className='info d-flex flex-column'>
+                    <span className='label'>Your Location</span>
+                    <span className='name'>India</span>
+                </div>
+                <span className='ml-auto'><FaAngleDown/></span>
+            </Button>
+            
+            <Dialog className='countryDialog' open={isCountryModal} onClose={()=>setisCountryModal(false)}
+              TransitionComponent={Transition}>
+                  <h5 className='countryDialogheader' >Choose your Delivery Location</h5>
+                  <p className='countryDialogDesc' >Enter your location and check service availability in your area.</p>
+                  <Button className='close_' onClick={()=>setisCountryModal(false)}><IoIosCloseCircle/></Button>
+                  <div className='headerSearch'>
+                          <input type='text' placeholder='Pick Your Location...'/>
+                          <Button><IoIosSearch/></Button>
+                   </div>
+                   <ul className='countryList mt-3'>
+                      <li><Button onClick={()=>setisCountryModal(false)}>Chennai</Button></li>
+                      <li><Button>Namakkal</Button></li>
+                      <li><Button>Thanjavur</Button></li>
+                      <li><Button>Kumbakonam</Button></li>
+                      <li><Button>Chennai</Button></li>
+                      <li><Button>Namakkal</Button></li>
+                      <li><Button>Thanjavur</Button></li>
+                      <li><Button>Kumbakonam</Button></li>   
+                      <li><Button>Chennai</Button></li>
+                      <li><Button>Namakkal</Button></li>
+                      <li><Button>Thanjavur</Button></li>
+                      <li><Button>Kumbakonam</Button></li>   
+                      <li><Button>Chennai</Button></li>
+                      <li><Button>Namakkal</Button></li>
+                      <li><Button>Thanjavur</Button></li>
+                      <li><Button>Kumbakonam</Button></li>                        
+                    </ul>
+            </Dialog>
         </div>
   )
 }
--- src/Components/Header/Navigation/index.js
+++ src/Components/Header/Navigation/index.js
@@ -10,28 +10,38 @@ import { FaMobileScreenButton } from "react-icons/fa6";
 import { MdOutlineLocalGroceryStore } from "react-icons/md";
 import { GrBlog } from "react-icons/gr";
 import { MdContactMail } from "react-icons/md";
+const db = require('countrycitystatejson')
+
 
 const Navigation = () => {
+
+  const getCities = () => {
+    console.log(db.getStatesByShort("IN"));
+    console.log(db.getCities('IN', 'Tamil Nadu'))
+    //console.log(Array.isArray(cities)); // Full list
+    //cities.flatMap(city => console.log(Array.isArray(city)))
+  }
+
   return (
     <nav>
           <div className='container'>
             <div className='row'>
               <div className='col-sm-2 catogertyTab1'>
-                <Button className='allCatTab'>
+                <Button className='allCatTab' onClick={getCities}>
                   <span className='allCatIcon1'><IoIosMenu/></span>
                   <span class="text">All Categories</span>
                   <span className='allCatIcon2'><FaAngleDown/></span>
                 </Button>
               </div>
-              <div className='col-sm-9 catogertyTab2 d-flex align-items-center'>
+              <div className='col-sm-9 ml-5 catogertyTab2 d-flex align-items-center'>
                 <ul className='list list-inline ml-auto'>
-                    <li className='list-inline-item'><Link to="/"><BsShop/> &nbsp; Home</Link></li>
-                    <li className='list-inline-item'><Link to="/"><FaShopify/> &nbsp; Fashion</Link></li>
-                    <li className='list-inline-item'><Link to="/"><FaMobileScreenButton/> &nbsp; Electronics</Link></li>
-                    <li className='list-inline-item'><Link to="/"><GiChickenOven/> &nbsp; Foods</Link></li>
-                    <li className='list-inline-item'><Link to="/"><MdOutlineLocalGroceryStore/> &nbsp; Groceries</Link></li>
-                    <li className='list-inline-item'><Link to="/"><GrBlog/> &nbsp; Blog</Link></li>
-                    <li className='list-inline-item'><Link to="/"><MdContactMail/> &nbsp; Contact us</Link></li>
+                    <li className='list-inline-item'><Link to="/"><Button><BsShop/> &nbsp; Home</Button></Link></li>
+                    <li className='list-inline-item'><Link to="/"><Button><FaShopify/> &nbsp; Fashion</Button></Link></li>
+                    <li className='list-inline-item'><Link to="/"><Button><FaMobileScreenButton/> &nbsp; Electronics</Button></Link></li>
+                    <li className='list-inline-item'><Link to="/"><Button><GiChickenOven/> &nbsp; Foods</Button></Link></li>
+                    <li className='list-inline-item'><Link to="/"><Button><MdOutlineLocalGroceryStore/> &nbsp; Groceries</Button></Link></li>
+                    <li className='list-inline-item'><Link to="/"><Button><GrBlog/> &nbsp; Blog</Button></Link></li>
+                    <li className='list-inline-item'><Link to="/"><Button><MdContactMail/> &nbsp; Contact us</Button></Link></li>
                 </ul>
               </div>
             </div>
